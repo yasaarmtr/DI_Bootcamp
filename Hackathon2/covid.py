9413734095cb02833a13bf8ca59e974a8532dd40
Yasaar Mauthoor <91458263+yasaarmtr@users.noreply.github.com>
2021-12-20 01:17:23 +0400

    v2

--- Hackathon2/covid.py
+++ Hackathon2/covid.py
@@ -4,14 +4,13 @@ Created on Sat Dec 11 19:32:42 2021
 
 @author: Yasaar Mauthoor
 """
-
-# import requests
+"""trials"""
 
 # requests.get ("https://api.covid19api.com/dayone/country/south-africa/status/confirmed")
 
 # response = requests.get("http://api.open-notify.org/iss-now.json")
-
-import requests
+# import numpy as np
+# import requests
 # import json
 
 # url = 'https://api.covid19api.com/dayone/country/mauritius/status/recovered/live' # confirmed cases only since the begining
@@ -27,37 +26,42 @@ import requests
 # Date = data["Countries"][110]["Date"]
 # print (TotalConfirmed)
 # print (Date[:10])
+# ---------------------------------------------------------------------------------------
 
-
-
+import requests
 
 """Confirmed cases"""
 url_confirmed = 'https://api.covid19api.com/dayone/country/mauritius/status/confirmed/live'
 data_confirmed = (requests.get(url_confirmed)).json()
-# for i in data_confirmed:
-case_confirmed = data_confirmed[0]["Cases"]
+for i in range(10): 
+    case_confirmed = data_confirmed[i]["Cases"]
+    date = data_confirmed[i]["Date"]
+    print (case_confirmed, date)
+next
     
 
 
-"""Deaths cases"""
-url_deaths = 'https://api.covid19api.com/dayone/country/mauritius/status/deaths/live'
-data_deaths = (requests.get(url_deaths)).json()
-
-case_deaths = data_deaths[0]["Cases"]
-
-"""Recovered cases"""
-url_recovered = 'https://api.covid19api.com/dayone/country/mauritius/status/recovered/live'
-data_recovered = (requests.get(url_recovered)).json()
-
-case_recovered = data_recovered[0]["Cases"]
-
-
-#function retrive data
 
 
 
 
-# """
-# GETLive By Country And Status
-# GETLive By Country All Status
-# GETive By Country And Status After Dat"""
+
+
+
+#--------------------------------------------------------------
+
+# """Deaths cases"""
+# url_deaths = 'https://api.covid19api.com/dayone/country/mauritius/status/deaths/live'
+# data_deaths = (requests.get(url_deaths)).json()
+
+# case_deaths = data_deaths[0]["Cases"]
+
+# """Recovered cases"""
+# url_recovered = 'https://api.covid19api.com/dayone/country/mauritius/status/recovered/live'
+# data_recovered = (requests.get(url_recovered)).json()
+
+# case_recovered = data_recovered[0]["Cases"]
+
+
+
+
